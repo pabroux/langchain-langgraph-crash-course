8b8755b3ab8b53fb41eefc56378ed8d0e9367837
pabroux <pierre-alexandrebroux@live.fr>
2025-07-07 11:24:42 +0200

    feat(langgraph): add `rag_advanced.py`.

---
+++ langgraph/rag/rag_advanced.py
@@ -0,0 +1,172 @@
+import os
+from typing import List
+
+import bs4
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.documents import Document
+from langchain_core.messages import SystemMessage
+from langchain_core.tools import tool
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from typing_extensions import TypedDict
+
+from langchain import hub
+from langchain.chat_models import init_chat_model
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import END, MessagesState, StateGraph
+from langgraph.prebuilt import ToolNode, create_react_agent, tools_condition
+
+os.environ["OPENAI_API_KEY"] = input("OpenAI API key: ")
+
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
+
+# Memory
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
+
+# ↳ Specify an ID for the thread
+config = {"configurable": {"thread_id": "vForVendetta501"}}
+
+
+# Load and chunk contents of the blog
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+all_splits = text_splitter.split_documents(docs)
+
+
+# Add documents to the vector store
+_ = vector_store.add_documents(documents=all_splits)
+
+
+# Define the state for the application
+graph_builder = StateGraph(MessagesState)
+
+
+# Turn the retrieve step into a tool call option for the LLM
+@tool(response_format="content_and_artifact")
+def retrieve(query: str):
+    """Retrieve information related to a query."""
+    retrieved_docs = vector_store.similarity_search(query, k=2)
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\nContent: {doc.page_content}")
+        for doc in retrieved_docs
+    )
+    return serialized, retrieved_docs
+
+
+# Define the nodes
+# ↳ A node that fields the user input, either generating a query
+#   for the retriever or responding directly
+def query_or_respond(state: MessagesState):
+    """Generate tool call for retrieval or respond."""
+    llm_with_tools = llm.bind_tools([retrieve])
+    response = llm_with_tools.invoke(state["messages"])
+    # MessagesState appends messages to state instead of overwriting
+    return {"messages": [response]}
+
+
+# ↳ A node for the retriever tool that executes the retrieval step
+tools = ToolNode([retrieve])
+
+
+# ↳ A node that generates the final response using the retrieved
+#   context
+def generate(state: MessagesState):
+    """Generate answer."""
+    # Get generated ToolMessages
+    recent_tool_messages = []
+    for message in reversed(state["messages"]):
+        if message.type == "tool":
+            recent_tool_messages.append(message)
+        else:
+            break
+    tool_messages = recent_tool_messages[::-1]
+
+    # Format into prompt
+    docs_content = "\n\n".join(doc.content for doc in tool_messages)
+    system_message_content = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        f"{docs_content}"
+    )
+    conversation_messages = [
+        message
+        for message in state["messages"]
+        if message.type in ("human", "system")
+        or (message.type == "ai" and not message.tool_calls)
+    ]
+    prompt = [SystemMessage(system_message_content)] + conversation_messages
+
+    # Run
+    response = llm.invoke(prompt)
+    return {"messages": [response]}
+
+
+# Build the graph
+graph_builder.add_node(query_or_respond)
+graph_builder.add_node(tools)
+graph_builder.add_node(generate)
+
+graph_builder.set_entry_point("query_or_respond")
+graph_builder.add_conditional_edges(
+    "query_or_respond",
+    tools_condition,
+    {END: END, "tools": "tools"},
+)
+graph_builder.add_edge("tools", "generate")
+graph_builder.add_edge("generate", END)
+
+graph = graph_builder.compile()
+
+
+# Test it out
+input_message = "What is Task Decomposition?"
+
+for step in graph.stream(
+    {"messages": [{"role": "user", "content": input_message}]},
+    stream_mode="values",
+    config=config,
+):
+    step["messages"][-1].pretty_print()
+
+
+# Using a ReAct agent
+# ↳ All the previous steps can be replaced with a ReAct agent by
+#   using the `create_react_agent` function. The only difference
+#   is that instead of a final generation step that ends the
+#   run, here the tool invocation loops back to the original LLM
+#   call. The model can then either answer the question using the
+#   retrieve, or generate another tool call to obtain more
+#   information
+agent_executor = create_react_agent(llm, [retrieve], checkpointer=memory)
+
+# ↳ Test it out
+config = {"configurable": {"thread_id": "delorean1989"}}
+
+input_message = (
+    "What is the standard method for Task Decomposition?\n\n"
+    "Once you get the answer, look up common extensions of that method."
+)
+
+for event in agent_executor.stream(
+    {"messages": [{"role": "user", "content": input_message}]},
+    stream_mode="values",
+    config=config,
+):
+    event["messages"][-1].pretty_print()
